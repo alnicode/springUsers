557981b0cd80ad036f69777103c278f42596ddc4
alnicode32 <albensnico@gmail.com>
2022-02-28 01:17:45 -0500

    Añadida la anotación Repository en UserRepository

--- src/main/java/com/alnicode/single/users/repositories/UserRepository.java
+++ src/main/java/com/alnicode/single/users/repositories/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import com.alnicode.single.users.models.UserModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserModel, Long> {
     Optional<List<UserModel>> findByAge(int age);
 }
